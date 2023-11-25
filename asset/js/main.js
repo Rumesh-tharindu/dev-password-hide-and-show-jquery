7f7401a3bf009218532a780ce977439c042ccaf1
amarsinghe <rumesh@silverstripers.com>
2023-11-25 18:45:12 +0530

    init starter

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,17 +1,3 @@
 $(document).ready(function () {
-	const passwordBtn = $('#passwordToText');
-	const passwordInput = $('#password');
-	if (passwordBtn) {
-		passwordBtn.on('click', function () {
-			if (passwordInput.attr('type') == 'password') {
-				passwordInput.attr('type', 'text');
-				$('#passwordToText i').removeClass('fa-eye-slash');
-				$('#passwordToText i').addClass('fa-eye');
-			} else {
-				passwordInput.attr('type', 'password');
-				$('#passwordToText i').removeClass('fa-eye');
-				$('#passwordToText i').addClass('fa-eye-slash');
-			}
-		});
-	}
+	console.log('Hey this my main js file');
 });
